c70986f82bcbb4cd76ffa445d17a38ee9f177280
Mark John Buenconsejo <hi@markjeee.com>
2018-04-22 14:05:31 +0800

    Hello World

---
+++ Gemfury.HelloDotNet/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Gemfury.WorldDotNet;
+
+namespace Gemfury.HelloDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hello hello = new Hello();
+            Console.WriteLine(string.Format("Hi, {0}", hello.SayHello()));
+        }
+    }
+}
